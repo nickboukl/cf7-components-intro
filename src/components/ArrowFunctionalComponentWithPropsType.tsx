0bfe033c7c59b2d7094f29b06fe4de9b90b461bd
Nik. Bouklas <nik.bouklas@gmail.com>
2025-05-20 21:01:38 +0300

    feat: new components created

---
+++ src/components/ArrowFunctionalComponentWithPropsType.tsx
@@ -0,0 +1,32 @@
+// type Props = {
+//     title: string;
+//     description: string;
+// }
+
+// type A = {
+//     title: string;
+// }
+// type B = {
+//     description: string;
+// }
+//
+// type MergedProps = A & B
+
+interface Props {
+    title: string;
+}
+
+interface Props {
+    description: string;
+}
+
+const ArrowFunctionalComponentWithProps = ({title, description}: Props) => {
+    return (
+        <>
+            <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>;
+            <p className="text-center text-gray-800">{description}</p>
+        </>
+    )
+}
+
+export default ArrowFunctionalComponentWithProps
---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,6 @@
+function FunctionalComponent() {
+    const title = 'Is a Functional Component!';
+    return <h1 className="text-center">{title}</h1>
+}
+
+export default FunctionalComponent;
